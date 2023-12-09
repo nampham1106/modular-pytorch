dc4a51a3f2ca623729c57b0c2c17546ce60a26ab
nampham1106 <nampham11062002@gmail.com>
2023-12-09 12:21:45 +0700

    uploaded file helper

---
+++ data_setup.py
@@ -0,0 +1,35 @@
+import os
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+NUM_WORKERS = os.cpu_count()
+
+def create_dataloaders(
+    train_dir: str,
+    test_dir: str,
+    transform: transforms.Compose,
+    batch_size: int,
+    num_workers: int=NUM_WORKERS
+):
+    train_data = datasets.ImageFolder(root=train_dir,
+                                      transform=transform)
+    test_data = datasets.ImageFolder(root=test_data,
+                                     transform=transform)
+    
+    class_names = train_data.classes
+    
+    train_dataloader = DataLoader(dataset=train_data,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  num_workers=num_workers,
+                                  pin_memory=True)
+    
+    train_dataloader = DataLoader(dataset=test_data,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  num_workers=num_workers,
+                                  pin_memory=True)
+    
+    
+    return train_dataloader, test_dataloader, class_names
