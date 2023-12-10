bb96409c9cc40efc0cfc44c49866fe3845ed357c
nampham1106 <nampham11062002@gmail.com>
2023-12-10 11:58:39 +0700

    fix bug in helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -100,30 +100,32 @@ def pred_and_plot_image(
     transform=None,
     device:torch.device="cuda" if torch.cuda.is_available() else "cpu"
 ):
-    target_image = torchvision.io.read_image(image_path).type(torch.float32)
+    ttarget_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
+
     target_image = target_image / 255.0
-    
+
     if transform:
         target_image = transform(target_image)
-    
+
     model.to(device)
+
     model.eval()
     with torch.inference_mode():
-        target_image = target_image.unqueeze(dim=0)
-        target_image_pred = model(target_image)
-        
-    target_image_pred_prob = torch.softmax(target_image_pred, dim=1)
-    target_image_pred_label = torch.argmax(target_image_pred_prob, dim=1)
-    
+        target_image = target_image.unsqueeze(dim=0)
+
+        target_image_pred = model(target_image.to(device))
+
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
     plt.imshow(
         target_image.squeeze().permute(1, 2, 0)
-    )    
-    
+    )  # make sure it's the right size for matplotlib
     if class_names:
-        title = f"Pred: {class_names[target_image_pred_label.cpu()]} | prob: {target_image_pred_prob.max().cpu()}"
+        title = f"Pred: {class_names[target_image_pred_label.cpu()]} | Prob: {target_image_pred_probs.max().cpu():.3f}"
     else:
-        title = f"Pred: {target_image_pred_label.cpu()} | prob: {target_image_pred_prob.max().cpu()}"
-    
+        title = f"Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}"
     plt.title(title)
     plt.axis(False)
 
